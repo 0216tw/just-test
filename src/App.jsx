7c2a98a03f6b0124c0f87831bec99138b8d1067a
Lab4DX <Lab4DX@gmail.com>
2026-08-13 14:13:55 +0900

    서버변경22222222

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
         <div className="server-badge" title=".env.local의 VITE_API_BASE_URL 값입니다.">
           <span className="server-dot" />
           <div>
-            <small>크크크배고파밥줘</small>
+            <small>너도?아나도</small>
             <strong>{API_BASE_URL}</strong>
           </div>
         </div>
